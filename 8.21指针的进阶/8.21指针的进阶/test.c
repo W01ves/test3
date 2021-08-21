ea3947812541814a4a0d208e38837c6abfeb2477
wwh <573898456@qq.com>
2021-08-21 20:53:33 +0800

    learning

---
+++ 8.21指针的进阶/8.21指针的进阶/test.c
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//void cat(int** p)
+//{
+//	printf("%p", *p);
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int *p = &a;
+//	int **pp = &p;
+//	cat(pp);
+//	return 0;
+//}
+
+//int add(int x, int y)
+//{
+//	int z = x + y;
+//	return z;	
+//
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int (*p)(int, int) = add;       //  定义函数指针       &函数名  和 函数名都是函数的地址
+//	                               //  定义方法与数组类似
+//	printf("%d\n", (*p)(a, b));     
+//	return 0;
+//}
+
+
+//void print(char* str)
+//{
+//	printf("%s\n", str);
+//}
+//
+//int main() 
+//{
+//	char arr[] = "abcdef";
+//	void (*p)(char*) = print;
+//	(*p)(arr);
+//	return 0;
+//}
+
+
+
+
