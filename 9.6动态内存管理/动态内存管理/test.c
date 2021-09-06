24638ec735cc432a98d5c5569dbe173986ed71c3
wwh <573898456@qq.com>
2021-09-06 21:23:44 +0800

    learning

---
+++ 9.6动态内存管理/动态内存管理/test.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+
+//struct S
+//{
+//	char name[20];
+//	int age;
+//};
+//
+//
+//int main()
+//{
+//	struct S arr[50];//50个 struct S类型的数据
+//	//30 浪费
+//	//60 不够
+//
+//
+//	return 0;
+//}
+
+
+//C 可以创建变长数组 - C99中增加了
