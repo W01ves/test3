d04a15c73c57df41d50dc28f21389bb80d8bab41
wwh <573898456@qq.com>
2021-09-24 22:08:19 +0800

    learning

---
+++ 9.24预处理操作/9.24预处理操作/test.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main()
+{
+	int arr[10] = { 0 };
+	int i = 0;	
+	for ( i = 0; i < 10; i++)
+	{
+		arr[i] = i;
+
+	}
+
+	for ( i = 0; i <  10; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+
+
+	return 0;
+}
