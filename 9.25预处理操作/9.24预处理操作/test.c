e1513d6448092bbeee19a314cc8a123474367452
wwh <573898456@qq.com>
2021-09-25 15:02:58 +0800

    learning

---
+++ 9.25预处理操作/9.24预处理操作/test.c
@@ -0,0 +1,132 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int i = 0;	
+//	for ( i = 0; i < 10; i++)
+//	{
+//		arr[i] = i;
+//
+//	}
+//
+//	for ( i = 0; i <  10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//
+//
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	printf("%s\n", __FILE__);
+//	printf("%d\n", __LINE__);
+//	printf("%s\n", __DATE__);
+//	printf("%s\n", __TIME__);
+//	//printf("%s\n", __STDC__);
+//
+//
+//
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	FILE* pf = fopen("test.txt", "w");
+//	if (pf = NULL)
+//	{
+//		perror("pf");
+//		exit( );
+//	}
+//
+//
+//	fclose(pf);
+//	pf = NULL;
+//
+//	return 0;
+//}
+
+
+
+//#define SQUARE(X) X*X	//ºê
+//
+//int main()
+//{
+//	int ret = SQUARE(5);		//25   5*5
+//	int ret1 = SQUARE(5+1);		//11   5+1*5+1
+//	printf("%d",ret);
+//
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	//int a = 0;
+//	//printf("the value of a is %d\n", a);
+//
+//	printf("hello world\n");
+//	printf("hello " "world\n");
+//	return 0;
+//}
+
+
+//#define PRINT(X) printf("the value of " #X " is %d\n", X)
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	PRINT(a);
+//	PRINT(b);
+//
+//
+//	return 0;
+//}
+
+//
+//#define CAT(X,Y) X##Y
+//
+//int main()
+//{
+//	int class84 = 2020;
+//	printf("%d\n", CAT(class, 84));
+//
+//	return 0;
+//
+//}
+
+
+
+
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch = fgetc(stdin)) != EOF)
+//	{
+//		printf("%c\n", ch+32);
+//	}
+//
+//
+//	return 0;
+//}
+
+
+int main()
+{
+	FILE* fp;
+	fflush(fp);
+	return 0;
+}
+
+
