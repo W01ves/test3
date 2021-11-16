8db1b3eced9433d7666cf1596f07222f836082fe
wwh <573898456@qq.com>
2021-11-16 22:44:36 +0800

    learning

---
+++ 11.16刷题/11.15刷题/test.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//bool isUnivalTree(struct TreeNode* root)
+//{
+//
+//	if (root)
+//	{
+//		if ((root->left != NULL) && root->val != root->left->val)
+//		{
+//			return 0;
+//		}
+//		else if ((root->right != NULL) && root->val != root->right->val)
+//		{
+//			return 0;
+//		}
+//		else
+//		{
+//			return isUnivalTree(root->left)*isUnivalTree(root->right);
+//		}
+//	}
+//
+//	return 1;
+//
+//}
+
+
+int main()
+{
+	printf("%.3x", '1');
+
+	return 0;
+}
